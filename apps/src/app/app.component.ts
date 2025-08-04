ddad0bbf31c598bc3df6689b2c3ed1c2163665fd
claire <iamclaire.cheng@gmail.com>
2025-08-04 15:27:20 +0200

    chore: update Angular and related dependencies to version 20.1.4, upgrade Nx packages to version 21.3.11, and update Jest and TypeScript versions

--- apps/src/app/app.component.ts
+++ apps/src/app/app.component.ts
@@ -17,29 +17,29 @@ type ContentsType = { type: GeminiType; content: string };
 export class AppComponent {
   #geminiService = inject(GeminiService);
   #$contents = signal<ContentsType | null>(null);
-  $resource = rxResource<string, ContentsType | null>(
-    {
-      request: computedWith(this.#$contents)
-        .skip(1)
-        .filter(request => request !== null && request.content.trim().length > 0)
-        .default(null),
-      // in the angular v20, the loader is replaced by stream
-      loader: (params) => {
-        if (params.request === null) return EMPTY;
-        const { type, content } = params.request;
-        switch (type) {
-          case 'generate':
-            return this.#geminiService.generateContent$(content);
-          case 'search':
-            return this.#geminiService.generateSearch$(content);
-          case 'chat':
-            return this.#geminiService.generateChat$(content);
-          default:
-            return EMPTY;
-        }
+
+  $requestSignal = computedWith(this.#$contents)
+    .skip(1)
+    .filter((request: ContentsType | null) => request !== null && request.content.trim().length > 0)
+    .default(null);
+
+  $resource = rxResource<string, ContentsType | null>({
+    params: () => this.$requestSignal(),
+    stream: (params) => {
+      if (params.params === null) return EMPTY;
+      const { type, content } = params.params;
+      switch (type) {
+        case 'generate':
+          return this.#geminiService.generateContent$(content);
+        case 'search':
+          return this.#geminiService.generateSearch$(content);
+        case 'chat':
+          return this.#geminiService.generateChat$(content);
+        default:
+          return EMPTY;
       }
     }
-  );
+  });
 
   protected executeGemini(type: GeminiType, content: string): void {
     this.#$contents.set({ type, content });
--- apps/src/app/app.config.ts
+++ apps/src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { loggingInterceptor } from '../interceptors';
 import { appRoutes } from './routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideExperimentalZonelessChangeDetection(),
+    provideZonelessChangeDetection(),
     provideRouter(appRoutes),
     provideHttpClient(withFetch(), withInterceptors([loggingInterceptor])),
   ]
